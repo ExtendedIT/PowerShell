f9f1ac79c7ba5be440806b402998638bb779f5c2
ExtendedIT <37689878+ExtendedIT@users.noreply.github.com>
2018-05-23 09:13:44 +0700

    Initial Upload

---
+++ VPN-Reconnection/VPN-Reconnection/Script.ps1
@@ -0,0 +1,11 @@
+﻿$vpnName = "<VPN Name>";
+    $vpn = Get-VpnConnection -Name $vpnName;
+    while($vpn.ConnectionStatus -eq "Disconnected"){
+    rasdial $vpnName "username" "password";
+    Start-Sleep -Seconds 120
+    }
+
+#Disconnect Event ID
+#rasman
+#20268
+#System
