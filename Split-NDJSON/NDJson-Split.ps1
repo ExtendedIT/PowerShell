9ee569466cc6b0908bdf6fe865a5707d90ecb4ed
ExtendedIT <Harrison@extendedit.co.uk>
2018-08-21 15:04:44 +0700

    Initial Push

---
+++ Split-NDJSON/NDJson-Split.ps1
@@ -0,0 +1,8 @@
+﻿$content = Get-Content "path to NDJSON file goes her"
+
+foreach ($data in $content){
+
+    $random = -join ((65..90) + (97..122) | Get-Random -Count 25 | % {[char]$_})
+    $data | out-file "<output folder goes here>\$random.json"
+
+}
